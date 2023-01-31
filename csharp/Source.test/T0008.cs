cd5f33b18f7391bec99b0842bdc2ed0b9157b01f
yutang27 <yutangmaster@gmail.com>
2023-01-31 21:37:39 +0800

    add num0008

---
+++ csharp/Source.test/T0008.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Source;
+
+namespace Source.Tests;
+
+[TestClass]
+public class T0008
+{
+    [TestMethod]
+    [DataRow("  42", 42)]
+    public void TestMethod1(string input0, int expected)
+    {
+        int res = N0008StringToInteger.MyAtoi(input0);
+        Assert.AreEqual(res, expected);
+    }
+
+    [TestMethod]
+    [DataRow("-4212re", -4212)]
+    public void TestMethod2(string input0, int expected)
+    {
+        int res = N0008StringToInteger.MyAtoi(input0);
+        Assert.AreEqual(res, expected);
+    }
+
+    [TestMethod]
+    [DataRow("  323323323323323re", Int32.MaxValue)]
+    public void TestMethod3(string input0, int expected)
+    {
+        int res = N0008StringToInteger.MyAtoi(input0);
+        Assert.AreEqual(res, expected);
+    }
+
+}
---
+++ csharp/Source/N0008StringToInteger.cs
@@ -0,0 +1,70 @@
+﻿namespace Source;
+
+public class N0008StringToInteger
+{
+    public static int MyAtoi(string s)
+    {
+        int res = 0;
+        int stage = 0;
+        bool isPositive = true;
+        int temp;
+
+        foreach (char c in s)
+        {
+            if (stage == 0)
+            {
+                switch (c)
+                {
+                    case ' ':
+                        continue;
+                    case '+':
+                        stage = 1;
+                        break;
+                    case '-':
+                        isPositive = false;
+                        stage = 1;
+                        break;
+                    default:
+                        if (c >= '0' && c <= '9')
+                        {
+                            res = c - '0';
+                            stage = 1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                        break;
+                }
+            }
+            else if (stage == 1)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    if ((res > Int32.MaxValue / 10) || (res < Int32.MinValue / 10))
+                    {
+                        return isPositive? Int32.MaxValue : Int32.MinValue;
+                    }
+
+                    res *=10;
+                    temp = isPositive ? res + (c - '0') : res - (c - '0');
+
+                    if(res!=0&&(temp < 0) ^ (res < 0))
+                    {
+                        return isPositive ? Int32.MaxValue : Int32.MinValue;
+                    }
+                    res = temp;
+                }
+                else
+                {
+                    stage = 2;
+                }
+            }
+            else
+            {
+                return res;
+            }
+        }
+        return res;
+    }
+}
